cdd77184db9de11373307ada5ce6a278e8159e3d
tamkeen99 <119537780+tamkeen99@users.noreply.github.com>
2024-05-23 10:38:04 +0530

    This is Updated Swaglab Project

---
+++ src/test/java/com/SwagLab/testScripts/Canmmit.java
@@ -0,0 +1,22 @@
+package com.SwagLab.testScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Canmmit {
+
+	
+	
+	
+	
+@Test
+	
+	
+	public void Cammitpehla() {
+		
+	Reporter.log("First method of Canmmit Class", true);
+	}
+
+
+}
